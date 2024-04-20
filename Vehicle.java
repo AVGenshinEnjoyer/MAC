5c5896a1a6f98dc7af399c73e530bd1d6d933016
AVGenshinEnjoyer <NEYOabout@gmail.com>
2024-04-21 02:41:06 +0500

    First commit

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+
+    public String model;
+    public String name;
+
+    Vehicle(String model, String name){
+
+        this.model = model;
+        this.name = name;
+    }
+}
---
+++ rp.java
@@ -0,0 +1,13 @@
+public class rp {
+
+    public static void main(String[] args) {
+
+
+
+    Vehicle vehicle = new Vehicle("Chiron", "Buga");
+
+        System.out.println(vehicle.model);
+        System.out.println(vehicle.name);
+
+    }
+}
